4c2397125e5a48f5cc6ded531085135801aa59cd
Anda Iulia Bratasanu <anda.bratasanu@gmail.com>
2024-10-19 18:33:53 +0300

    adaugat gandac provizoriu in scena player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerMovement:MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        float speed = 5f;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //transform.position += 
+            // addForce?
+        }
+    }
+}
