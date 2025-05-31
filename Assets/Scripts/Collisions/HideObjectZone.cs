fb777c3a8db918d533cd08994aa73b59395d0b28
Petrica81 <petricasimion69@gmail.com>
2025-05-31 20:24:56 +0300

    pipes

---
+++ Assets/Scripts/Collisions/HideObjectZone.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class HideObjectZone : MonoBehaviour
+{
+    public GameObject[] targetObjects; // Listează toate obiectele în Inspector
+
+    void Start()
+    {
+        foreach (GameObject obj in targetObjects)
+        {
+            Renderer r = obj.GetComponent<Renderer>();
+            Collider c = obj.GetComponent<Collider>();
+            if (r != null) r.enabled = true;
+            if (c != null) c.enabled = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (GameObject obj in targetObjects)
+            {
+                Renderer r = obj.GetComponent<Renderer>();
+                Collider c = obj.GetComponent<Collider>();
+                if (r != null) r.enabled = false;
+                if (c != null) c.enabled = false;
+            }
+        }
+    }
+}
